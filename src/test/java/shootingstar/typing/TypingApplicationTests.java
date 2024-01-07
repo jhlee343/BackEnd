33dddb0fe5e055e1952af0f44cec779660357d18
reenact_l <77737814+jhlee343@users.noreply.github.com>
2024-01-08 00:46:17 +0900

    modify

--- src/test/java/shootingstar/typing/TypingApplicationTests.java
+++ src/test/java/shootingstar/typing/TypingApplicationTests.java
@@ -2,6 +2,7 @@ package shootingstar.typing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import shootingstar.typing.repository.TextRepository;
 
 @SpringBootTest
 class TypingApplicationTests {
