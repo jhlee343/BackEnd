f4d72c221b75847690d23ada463ad5406b567205
dnjs2721 <skadu66@gmail.com>
2024-01-25 00:24:04 +0900

    CORP 설정 추가, csv 파일 최신화

---
+++ src/main/java/shootingstar/typing/config/WebConfig.java
@@ -0,0 +1,25 @@
+package shootingstar.typing.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${allowList}")
+    private String[] allowList;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowList)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
